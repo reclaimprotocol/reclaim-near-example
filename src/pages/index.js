09f3abb3ed0f04a8a45bfac04c845c159b36926f
Hadi Saleh <hadi@creatoros.co>
2024-11-12 21:46:14 +0300

    update: using js-sdk v2.1.1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,35 @@
-import styles from '@/styles/app.module.css';
-import { useState, useEffect, useContext } from 'react';
-import { ReclaimNearContract, SMOKE_PROOF } from '../config';
-import { NearContext } from '@/context';
-import { Navigation } from '@/components/navigation';
-import {Reclaim} from '@reclaimprotocol/js-sdk';
-import { CreateNewProof } from '@/components/create-new-proof';
-
+import styles from "@/styles/app.module.css";
+import { useState, useEffect, useContext } from "react";
+import { ReclaimNearContract, SMOKE_PROOF } from "../config";
+import { NearContext } from "@/context";
+import { Navigation } from "@/components/navigation";
+import { transformForOnchain } from "@reclaimprotocol/js-sdk";
+import { CreateNewProof } from "@/components/create-new-proof";
 
 // Contract that the app will interact with
 const CONTRACT = ReclaimNearContract;
 
-
 export default function Home() {
   const { signedAccountId, wallet } = useContext(NearContext);
-  const [epoch, setEpoch] = useState('loading...');
-  const [newGenProof, setNewGenProof] = useState({})
-  const [readyToVerify, setReadyToVerify] = useState(false)
+  const [epoch, setEpoch] = useState("loading...");
+  const [newGenProof, setNewGenProof] = useState({});
+  const [readyToVerify, setReadyToVerify] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
   const [showSpinner, setShowSpinner] = useState(false);
 
   useEffect(() => {
     if (!wallet) return;
 
-    wallet.viewMethod({ contractId: CONTRACT, method: 'get_epoch_by_id', args: { epoch_id : "1"} }).then(
-      epoch => {
-        console.log(epoch)
-        setEpoch(epoch)}
-    );
+    wallet
+      .viewMethod({
+        contractId: CONTRACT,
+        method: "get_epoch_by_id",
+        args: { epoch_id: "1" },
+      })
+      .then((epoch) => {
+        console.log(epoch);
+        setEpoch(epoch);
+      });
   }, [wallet]);
 
   useEffect(() => {
@@ -35,7 +38,7 @@ export default function Home() {
 
   const verifyProof = async (proof) => {
     setShowSpinner(true);
-    const newProof = Reclaim.transformForOnchain(proof)
+    const newProof = transformForOnchain(proof);
     newProof.signedClaim.signatures[0] =
       newProof.signedClaim.signatures[0].replace("0x", "");
     newProof.signedClaim.claim.owner = newProof.signedClaim.claim.owner.replace(
@@ -44,49 +47,55 @@ export default function Home() {
     );
     newProof.signedClaim.claim.identifier =
       newProof.signedClaim.claim.identifier.replace("0x", "");
-    const tx = await wallet.callMethod({ contractId: CONTRACT, method: 'verify_proof', args: { proof: newProof } });
-    console.log(tx)
+    const tx = await wallet.callMethod({
+      contractId: CONTRACT,
+      method: "verify_proof",
+      args: { proof: newProof },
+    });
+    console.log(tx);
     setTransaction(tx);
     setShowSpinner(false);
   };
 
-
   return (
     <>
-    <div hidden={!loggedIn}>
-      <Navigation />
-    </div>
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Interacting with the contract: &nbsp;
-          <code className={styles.code}>Reclaim Contract on Near - Testnet</code>
-        </p>
+      <div hidden={!loggedIn}>
+        <Navigation />
       </div>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            Interacting with the contract: &nbsp;
+            <code className={styles.code}>
+              Reclaim Contract on Near - Testnet
+            </code>
+          </p>
+        </div>
 
-      <div>
-        <h1 className="w-100">
-          The contract says - Current Epoch
-        </h1>
-        {
-          typeof epoch == 'string' ? <div>{epoch}</div> :
+        <div>
+          <h1 className="w-100">The contract says - Current Epoch</h1>
+          {typeof epoch == "string" ? (
+            <div>{epoch}</div>
+          ) : (
             <div>
               <code>
                 id : {epoch.id} <br />
               </code>
               <code>
-                Minimum Witnesses for Claim Creation: {epoch.minimum_witness_for_claim_creation} <br />
+                Minimum Witnesses for Claim Creation:{" "}
+                {epoch.minimum_witness_for_claim_creation} <br />
               </code>
-              <code>
-                Witnesses: { epoch.witnesses.map(w => w.address)}
-              </code>
+              <code>Witnesses: {epoch.witnesses.map((w) => w.address)}</code>
             </div>
-        }
-      </div>
+          )}
+        </div>
 
-      <div hidden={!loggedIn}>
+        <div hidden={!loggedIn}>
           <div className={styles.col}>
-            <button className="btn btn-secondary" onClick={() => verifyProof(SMOKE_PROOF)}>
+            <button
+              className="btn btn-secondary"
+              onClick={() => verifyProof(SMOKE_PROOF)}
+            >
               <span hidden={showSpinner}> Verify Proof with MOCK_PROOF </span>
               <i
                 className="spinner-border spinner-border-sm"
@@ -94,10 +103,16 @@ export default function Home() {
               ></i>
             </button>
             <div hidden={readyToVerify}>
-              <CreateNewProof setNewProof={setNewGenProof} setReadyToVerify={setReadyToVerify} />
+              <CreateNewProof
+                setNewProof={setNewGenProof}
+                setReadyToVerify={setReadyToVerify}
+              />
             </div>
             <div hidden={!readyToVerify} className={styles.description}>
-              <button className="btn btn-secondary" onClick={() => verifyProof(newGenProof)}>
+              <button
+                className="btn btn-secondary"
+                onClick={() => verifyProof(newGenProof)}
+              >
                 <span hidden={showSpinner}> Verify New Generated Proof </span>
                 <i
                   className="spinner-border spinner-border-sm"
@@ -111,7 +126,7 @@ export default function Home() {
           <p className="m-0"> Please login to verify proofs </p>
           <Navigation />
         </div>
-    </main>
+      </main>
     </>
   );
 }
